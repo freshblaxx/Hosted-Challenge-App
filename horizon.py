35dfcd2651fdb552fd94511d20727902a4ee7d4c
freshblaxx <dominik.fabian.frank@gmail.com>
2025-12-01 10:26:27 -0600

    new

--- horizon.py
+++ horizon.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-from prophet import Prophet
+
 
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
